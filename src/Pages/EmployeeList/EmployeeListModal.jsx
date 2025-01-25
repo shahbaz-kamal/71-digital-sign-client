1d76f6ed1881668f2e3b91cb7381ffe5e76ce7f8
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-01-25 20:56:37 +0600

    payment for hr setup done functionality should be done after creating payment gateway

--- src/Pages/EmployeeList/EmployeeListModal.jsx
+++ src/Pages/EmployeeList/EmployeeListModal.jsx
@@ -1,11 +1,130 @@
-import React from 'react';
+import React from "react";
 
-const EmployeeListModal = () => {
-    return (
-        <div>
-            
+const EmployeeListModal = ({
+  refetch,
+  isModalOpen,
+  setIsModalOpen,
+  selectedUser,
+}) => {
+  if (!selectedUser) {
+    return null;
+  }
+
+  const {
+    name,
+    email,
+    isVerified,
+    bankAccount,
+    salary,
+    profilePhoto,
+    designation,
+  } = selectedUser;
+
+  //   handling payment request
+
+  const handlePaymentRequest = async (e) => {
+    e.preventDefault();
+    console.log("payment request triggered");
+  };
+  return (
+    <dialog open={isModalOpen} className="modal">
+      <div className="modal-box bg-muted-green">
+        <form
+          onSubmit={handlePaymentRequest}
+          className="card-body   rounded-2xl w-full mx-auto grid grid-cols-1 "
+        >
+          {/* Tasks */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-color-text">Salary</span>
+            </label>
+
+            <input
+              defaultValue={salary}
+              name="salary"
+              type="number"
+              placeholder="salary"
+              step={"0.01"}
+              className="input input-bordered"
+              required
+            />
+          </div>
+          {/* Month */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-color-text">Month</span>
+            </label>
+            <select
+              name="salary"
+              defaultValue="Pick a Month"
+              className="select select-ghost w-full bg-white"
+            >
+              <option disabled value="Pick a Month">
+                Pick a Month
+              </option>
+              <option value="January">January</option>
+              <option value="February">February</option>
+              <option value="March">March</option>
+              <option value="April">April</option>
+              <option value="May">May</option>
+              <option value="June">June</option>
+              <option value="July">July</option>
+              <option value="August">August</option>
+              <option value="September">September</option>
+              <option value="October">October</option>
+              <option value="November">November</option>
+              <option value="December">December</option>
+            </select>
+          </div>
+          {/* Month */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-color-text">Pick a year</span>
+            </label>
+            <select
+              name="salary"
+              defaultValue="Pick a Year"
+              className="select select-ghost w-full bg-white"
+            >
+              <option disabled value="Pick a Month">
+                Pick a Year
+              </option>
+              <option value="2013">2013</option>
+              <option value="2014">2014</option>
+              <option value="2015">2015</option>
+              <option value="2016">2016</option>
+              <option value="2017">2017</option>
+              <option value="2018">2018</option>
+              <option value="2019">2019</option>
+              <option value="2020">2020</option>
+              <option value="2021">2021</option>
+              <option value="2022">2022</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
+            </select>
+          </div>
+
+          <div className="form-control mt-4 ">
+            <button className=" py-2 px-3 rounded-md border border-primary text-color-text hover:bg-primary  transition ease-in-out duration-300 hover:text-white font-bold">
+              Make Payment Request
+            </button>
+          </div>
+        </form>
+        <div className="modal-action">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button
+              onClick={() => setIsModalOpen(false)}
+              className="btn input input-bordered bg-secondary border border-secondary hover:bg-secondary hover:border-muted-green hover:text-color-text transition ease-in-out duration-300 font-medium text-lg md:text-xl text-white"
+            >
+              Close
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    </dialog>
+  );
 };
 
 export default EmployeeListModal;
--- src/Pages/EmployeeList/EmployeeListTableRow.jsx
+++ src/Pages/EmployeeList/EmployeeListTableRow.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiEdit } from "react-icons/ci";
 import { MdVerified } from "react-icons/md";
 import { RxCross2 } from "react-icons/rx";
 import UseAxiosSecure from "../../Hooks/UseAxiosSecure";
 import Swal from "sweetalert2";
+import EmployeeListModal from "./EmployeeListModal";
 
 const EmployeeListTableRow = ({ singleEmployee, index, refetch }) => {
   const axiosSecure = UseAxiosSecure();
@@ -17,6 +18,13 @@ const EmployeeListTableRow = ({ singleEmployee, index, refetch }) => {
     profilePhoto,
     designation,
   } = singleEmployee;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  const handlePay = async () => {
+    setSelectedUser(singleEmployee);
+    setIsModalOpen(true);
+  };
   const handleVerified = async (_id) => {
     console.log(_id);
     const id = _id;
@@ -33,61 +41,74 @@ const EmployeeListTableRow = ({ singleEmployee, index, refetch }) => {
     }
   };
   return (
-    <tr>
-      <th className="text-center text-color-text text-sm md:text-base">
-        {index + 1}
-      </th>
-      <th className="text-center text-color-text text-sm md:text-base">
-        <div className="flex justify-center">
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle h-12 w-12">
-                <img src={profilePhoto} alt="Avatar Tailwind CSS Component" />
+    <>
+      <tr>
+        <th className="text-center text-color-text text-sm md:text-base">
+          {index + 1}
+        </th>
+        <th className="text-center text-color-text text-sm md:text-base">
+          <div className="flex justify-center">
+            <div className="flex items-center gap-3">
+              <div className="avatar">
+                <div className="mask mask-squircle h-12 w-12">
+                  <img src={profilePhoto} alt="Avatar Tailwind CSS Component" />
+                </div>
+              </div>
+              <div>
+                <div className="font-bold">{name}</div>
+                <div className="text-sm opacity-50">{designation}</div>
               </div>
             </div>
-            <div>
-              <div className="font-bold">{name}</div>
-              <div className="text-sm opacity-50">{designation}</div>
-            </div>
           </div>
-        </div>
-      </th>
-      <td className="text-center text-color-text text-sm md:text-base">
-        {email}
-      </td>
-      <td className="text-center text-color-text text-sm md:text-base">
-        <div className="flex justify-center ">
-          {isVerified ? (
-            <div className="bg-secondary bg-opacity-30 p-2 rounded-full text-neutral">
-              <MdVerified size={22} />
-            </div>
-          ) : (
-            <div
-              onClick={() => handleVerified(_id)}
-              className="bg-primary bg-opacity-30 p-2 rounded-full text-primary"
-            >
-              <RxCross2 size={22} />
-            </div>
-          )}
-        </div>
-      </td>
-      <td className="text-center text-color-text text-sm md:text-base">
-        {bankAccount}
-      </td>
-      <td className="text-center text-color-text text-sm md:text-base">
-        {salary}
-      </td>
-      <td className="text-center text-color-text text-sm md:text-base">
-        <button className="py-1 px-3 rounded-md border border-primary hover:bg-primary  transition ease-in-out duration-300 hover:text-white">
-          Pay
-        </button>
-      </td>
-      <td className="text-center text-color-text text-sm md:text-base">
-        <button className="py-1 px-3 rounded-md bg-secondary border border-secondary hover:bg-muted-green hover:border-muted-green hover:text-color-text transition ease-in-out duration-300">
-          Details
-        </button>
-      </td>
-    </tr>
+        </th>
+        <td className="text-center text-color-text text-sm md:text-base">
+          {email}
+        </td>
+        <td className="text-center text-color-text text-sm md:text-base">
+          <div className="flex justify-center ">
+            {isVerified ? (
+              <div className="bg-secondary bg-opacity-30 p-2 rounded-full text-neutral">
+                <MdVerified size={22} />
+              </div>
+            ) : (
+              <div
+                onClick={() => handleVerified(_id)}
+                className="bg-primary bg-opacity-30 p-2 rounded-full text-primary"
+              >
+                <RxCross2 size={22} />
+              </div>
+            )}
+          </div>
+        </td>
+        <td className="text-center text-color-text text-sm md:text-base">
+          {bankAccount}
+        </td>
+        <td className="text-center text-color-text text-sm md:text-base">
+          {salary}
+        </td>
+        <td className="text-center text-color-text text-sm md:text-base">
+          <button
+            onClick={handlePay}
+            className="py-1 px-3 rounded-md border border-primary hover:bg-primary  transition ease-in-out duration-300 hover:text-white"
+          >
+            Pay
+          </button>
+        </td>
+        <td className="text-center text-color-text text-sm md:text-base">
+          <button className="py-1 px-3 rounded-md bg-secondary border border-secondary hover:bg-muted-green hover:border-muted-green hover:text-color-text transition ease-in-out duration-300">
+            Details
+          </button>
+        </td>
+      </tr>
+      {singleEmployee && (
+        <EmployeeListModal
+          selectedUser={selectedUser}
+          setIsModalOpen={setIsModalOpen}
+          isModalOpen={isModalOpen}
+          refetch={refetch}
+        ></EmployeeListModal>
+      )}
+    </>
   );
 };
 
